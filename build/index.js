ddc593f1c28378ec9b534fe24803af3a1aa0b9be
Glo <glo.movil@gmail.com>
2023-02-18 11:04:23 +0100

    SP5 ex3 and ex4  updates

--- build/index.js
+++ build/index.js
@@ -1,4 +1,6 @@
 "use strict";
+// API Meteorología  ::::
+// AEMET OpenData es una API REST desarrollado por AEMET 
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,62 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const moodBtnsContainer = document.getElementById("moodBtnsContainer");
-if (moodBtnsContainer != null) {
-    moodBtnsContainer.style.visibility = "hidden";
-}
-const reportJokes = [];
-const moodButtons = document.querySelectorAll('[data-score]');
-let previousJoke = "";
-let moodScoreButton = "";
-function getJoke() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const myHeaders = new Headers({
-            "Accept": "application/json",
-            "User-Agent": "My Library (https://github.com/Leptonita/sprint5)"
-        });
-        yield fetch('https://icanhazdadjoke.com/', {
-            method: 'GET',
-            headers: myHeaders
-        })
-            .then(response => response.json())
-            .then(json => {
-            if (moodBtnsContainer != null) {
-                moodBtnsContainer.style.visibility = "visible";
-            }
-            const divJoke = document.querySelector("div .joke");
-            if (divJoke !== null) {
-                let currentJoke = json.joke;
-                //let lastScoreJoke = moodScoreButton[moodScoreButton.length - 1];
-                let lastScoreJoke = moodScoreButton;
-                getMoodScore();
-                const d = new Date();
-                reportJokes.push({ joke: previousJoke, score: lastScoreJoke, date: d.toISOString() });
-                previousJoke = currentJoke;
-                console.log(reportJokes);
-                return divJoke.innerHTML = json.joke;
-            }
-            else {
-                return alert("sorry, something went wrong! No joke, no fun");
-            }
-        });
-    });
-}
-function getMoodScore() {
-    moodScoreButton = "";
-    moodButtons.forEach(button => button.addEventListener('click', () => {
-        const moodScore = button.getAttribute("data-score");
-        console.log(moodScore);
-        if (moodScore !== null && (moodScore === "1" || moodScore === "2" || moodScore === "3")) {
-            moodScoreButton = moodScore;
-        }
-        else {
-            moodScoreButton = "";
-        }
-    }));
-}
-// API Meteorología :::: AEMET OpenData es una API REST desarrollado por AEMET 
-const urlApi = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/08019/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnbG9yaWFuZXRAZ21haWwuY29tIiwianRpIjoiYjRiYjgxMDUtMjUyNi00NTFjLWE1ZTUtM2E3ZGJhYzgxNTY1IiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2NzY1Nzg2MzUsInVzZXJJZCI6ImI0YmI4MTA1LTI1MjYtNDUxYy1hNWU1LTNhN2RiYWM4MTU2NSIsInJvbGUiOiIifQ.fVh7ThLMij0-ZuF2BHuUkEE95u1-XpNan0B3QJR3nw4";
+const urlMeteoApi = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/08019/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnbG9yaWFuZXRAZ21haWwuY29tIiwianRpIjoiYjRiYjgxMDUtMjUyNi00NTFjLWE1ZTUtM2E3ZGJhYzgxNTY1IiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2NzY1Nzg2MzUsInVzZXJJZCI6ImI0YmI4MTA1LTI1MjYtNDUxYy1hNWU1LTNhN2RiYWM4MTU2NSIsInJvbGUiOiIifQ.fVh7ThLMij0-ZuF2BHuUkEE95u1-XpNan0B3QJR3nw4";
 const theHeaders = {
     "accept": "application/json",
     "cache-control": "no-cache"
@@ -90,16 +37,24 @@ function getWeather(url) {
                 //estado del cielo por horas, dia[0] es hoy
                 const skyTodayHoursArr = todayPredictions.estadoCielo;
                 const temperatureTodayHoursArr = todayPredictions.temperatura;
-                console.log("hoy, prediccion cada hora del dia: estado del Cielo - Temperatura", skyTodayHoursArr, " - ", temperatureTodayHoursArr);
+                console.log(`Hoy, prediccion cada hora del dia:\n\n array estado del cielo  `, skyTodayHoursArr, "array temperatura: ", temperatureTodayHoursArr);
+                // hour must be a string, have two digits and be higher than 0 (they don't provide any value for 00:00, so we will get the prevision for the next hour)
                 const toDay = new Date();
-                const nowHour = toDay.getHours();
-                console.log(nowHour);
+                let nowHour = toDay.getHours().toString();
+                // nowHour = "1";
+                if (nowHour === "0") {
+                    nowHour = "01";
+                }
+                else if (Number(nowHour) < 10) {
+                    nowHour = "0" + nowHour;
+                }
+                console.log("nowHour: ", nowHour);
                 const skyThisHourArr = skyTodayHoursArr.filter((sItem) => {
                     console.log(typeof sItem);
-                    return sItem.periodo === nowHour.toString();
+                    return sItem.periodo === nowHour;
                 });
-                const tempThisHourArr = temperatureTodayHoursArr.filter((tItem) => tItem.periodo === nowHour.toString());
-                console.log("datos prediccion actual, en esta hora - Array: estado del Cielo - Temperatura", skyThisHourArr, " - ", tempThisHourArr);
+                const tempThisHourArr = temperatureTodayHoursArr.filter((tItem) => tItem.periodo === nowHour);
+                console.log(`datos para las ${nowHour} de hoy - Array: estado del Cielo - Temperatura`, skyThisHourArr, " - ", tempThisHourArr);
                 const skyDescriptionNow = skyThisHourArr[0].descripcion;
                 const temperatureNow = tempThisHourArr[0].value;
                 const messageWeather = `Hoy: cielo ${skyDescriptionNow.toLowerCase()} y ${temperatureNow} ºC`;
@@ -114,5 +69,66 @@ function getWeather(url) {
         });
     });
 }
-const dirApi = getWeather(urlApi);
-console.log("getWeather(urlApi)", dirApi);
+const dirApi = getWeather(urlMeteoApi);
+console.log("direction datos meteo AEMET, getWeather(urlMeteoApi)", dirApi);
+//
+//JOKES 
+// by icanhazdadjoke.com/
+const moodBtnsContainer = document.getElementById("moodBtnsContainer");
+if (moodBtnsContainer != null) {
+    moodBtnsContainer.style.visibility = "hidden";
+}
+const reportJokes = [];
+const moodButtons = document.querySelectorAll('[data-score]');
+let previousJoke = "";
+let moodScoreButton = "";
+function getJoke() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const urlDadJoke = "https://icanhazdadjoke.com/";
+        const myHeaders = new Headers({
+            "Accept": "application/json",
+            "User-Agent": "My Library (https://github.com/Leptonita/sprint5)"
+        });
+        yield fetch(urlDadJoke, {
+            method: 'GET',
+            headers: myHeaders
+        })
+            .then(response => response.json())
+            .then(json => {
+            if (moodBtnsContainer != null) {
+                moodBtnsContainer.style.visibility = "visible";
+            }
+            const divJoke = document.querySelector("div .joke");
+            if (divJoke !== null) {
+                let currentJoke = json.joke;
+                //let lastScoreJoke = moodScoreButton[moodScoreButton.length - 1];
+                let lastScoreJoke = moodScoreButton;
+                getMoodScore();
+                const d = new Date();
+                reportJokes.push({ joke: previousJoke, score: lastScoreJoke, date: d.toISOString() });
+                previousJoke = currentJoke;
+                console.log(reportJokes);
+                return divJoke.innerHTML = json.joke;
+            }
+            else {
+                return alert("sorry, something went wrong! No joke, no fun");
+            }
+        })
+            .catch(function (error) {
+            console.log("error", error);
+        });
+    });
+}
+function getMoodScore() {
+    moodScoreButton = "";
+    moodButtons.forEach(button => button.addEventListener('click', () => {
+        const moodScore = button.getAttribute("data-score");
+        console.log(moodScore);
+        if (moodScore !== null && (moodScore === "1" || moodScore === "2" || moodScore === "3")) {
+            moodScoreButton = moodScore;
+        }
+        else {
+            moodScoreButton = "";
+        }
+    }));
+}
--- index.ts
+++ index.ts
@@ -1,81 +1,7 @@
-const moodBtnsContainer: HTMLElement | null = document.getElementById("moodBtnsContainer");
+// API Meteorología  ::::
+// AEMET OpenData es una API REST desarrollado por AEMET 
 
-if (moodBtnsContainer != null) {
-    moodBtnsContainer.style.visibility = "hidden";
-}
-
-const reportJokes: { joke: string, score: string, date: string }[] = [];
-
-const moodButtons = document.querySelectorAll('[data-score]');
-
-let previousJoke: string = "";
-
-let moodScoreButton: string = "";
-
-
-async function getJoke() {
-    const myHeaders = new Headers({
-        "Accept": "application/json",
-        "User-Agent": "My Library (https://github.com/Leptonita/sprint5)"
-    });
-
-    await fetch('https://icanhazdadjoke.com/', {
-        method: 'GET',
-        headers: myHeaders
-    })
-        .then(response => response.json())
-        .then(json => {
-
-            if (moodBtnsContainer != null) {
-                moodBtnsContainer.style.visibility = "visible";
-            }
-
-            const divJoke: HTMLElement | null = document.querySelector("div .joke");
-
-            if (divJoke !== null) {
-
-                let currentJoke: string = json.joke;
-                //let lastScoreJoke = moodScoreButton[moodScoreButton.length - 1];
-                let lastScoreJoke = moodScoreButton;
-
-                getMoodScore();
-                const d = new Date();
-
-                reportJokes.push({ joke: previousJoke, score: lastScoreJoke, date: d.toISOString() });
-
-                previousJoke = currentJoke;
-                console.log(reportJokes);
-                return divJoke.innerHTML = json.joke;
-            } else {
-                return alert("sorry, something went wrong! No joke, no fun");
-            }
-
-
-
-        });
-}
-
-
-
-function getMoodScore() {
-    moodScoreButton = "";
-    moodButtons.forEach(button =>
-        button.addEventListener('click', () => {
-            const moodScore: string | null = button.getAttribute("data-score");
-            console.log(moodScore);
-            if (moodScore !== null && (moodScore === "1" || moodScore === "2" || moodScore === "3")) {
-                moodScoreButton = moodScore;
-            } else {
-                moodScoreButton = "";
-            }
-
-        }));
-
-}
-
-// API Meteorología :::: AEMET OpenData es una API REST desarrollado por AEMET 
-
-const urlApi: string = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/08019/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnbG9yaWFuZXRAZ21haWwuY29tIiwianRpIjoiYjRiYjgxMDUtMjUyNi00NTFjLWE1ZTUtM2E3ZGJhYzgxNTY1IiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2NzY1Nzg2MzUsInVzZXJJZCI6ImI0YmI4MTA1LTI1MjYtNDUxYy1hNWU1LTNhN2RiYWM4MTU2NSIsInJvbGUiOiIifQ.fVh7ThLMij0-ZuF2BHuUkEE95u1-XpNan0B3QJR3nw4";
+const urlMeteoApi: string = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/08019/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnbG9yaWFuZXRAZ21haWwuY29tIiwianRpIjoiYjRiYjgxMDUtMjUyNi00NTFjLWE1ZTUtM2E3ZGJhYzgxNTY1IiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2NzY1Nzg2MzUsInVzZXJJZCI6ImI0YmI4MTA1LTI1MjYtNDUxYy1hNWU1LTNhN2RiYWM4MTU2NSIsInJvbGUiOiIifQ.fVh7ThLMij0-ZuF2BHuUkEE95u1-XpNan0B3QJR3nw4";
 
 
 const theHeaders = {
@@ -106,11 +32,20 @@ async function getWeather(url: string) {
 
                     const skyTodayHoursArr = todayPredictions.estadoCielo;
                     const temperatureTodayHoursArr = todayPredictions.temperatura;
-                    console.log("hoy, prediccion cada hora del dia: estado del Cielo - Temperatura", skyTodayHoursArr, " - ", temperatureTodayHoursArr);
+                    console.log(`Hoy, prediccion cada hora del dia:\n\n array estado del cielo  `, skyTodayHoursArr,
+                        "array temperatura: ", temperatureTodayHoursArr);
 
+                    // hour must be a string, have two digits and be higher than 0 (they don't provide any value for 00:00, so we will get the prevision for the next hour)
                     const toDay = new Date();
-                    const nowHour = toDay.getHours();
-                    console.log(nowHour);
+                    let nowHour: string = toDay.getHours().toString();
+                    // nowHour = "1";
+                    if (nowHour === "0") {
+                        nowHour = "01";
+                    } else if (Number(nowHour) < 10) {
+                        nowHour = "0" + nowHour;
+                    }
+
+                    console.log("nowHour: ", nowHour);
 
                     interface Sitem {
                         value: string,
@@ -119,7 +54,7 @@ async function getWeather(url: string) {
                     }
                     const skyThisHourArr = skyTodayHoursArr.filter((sItem: Sitem) => {
                         console.log(typeof sItem);
-                        return sItem.periodo === nowHour.toString()
+                        return sItem.periodo === nowHour
                     });
 
                     interface Titem {
@@ -127,9 +62,9 @@ async function getWeather(url: string) {
                         periodo: string
                     }
 
-                    const tempThisHourArr = temperatureTodayHoursArr.filter((tItem: Titem) => tItem.periodo === nowHour.toString());
+                    const tempThisHourArr = temperatureTodayHoursArr.filter((tItem: Titem) => tItem.periodo === nowHour);
 
-                    console.log("datos prediccion actual, en esta hora - Array: estado del Cielo - Temperatura", skyThisHourArr, " - ", tempThisHourArr);
+                    console.log(`datos para las ${nowHour} de hoy - Array: estado del Cielo - Temperatura`, skyThisHourArr, " - ", tempThisHourArr);
 
                     const skyDescriptionNow = skyThisHourArr[0].descripcion;
                     const temperatureNow = tempThisHourArr[0].value;
@@ -145,6 +80,88 @@ async function getWeather(url: string) {
                 });
         })
 }
-const dirApi = getWeather(urlApi);
+const dirApi = getWeather(urlMeteoApi);
 
-console.log("getWeather(urlApi)", dirApi);
+console.log("direction datos meteo AEMET, getWeather(urlMeteoApi)", dirApi);
+
+//
+//JOKES 
+// by icanhazdadjoke.com/
+
+
+const moodBtnsContainer: HTMLElement | null = document.getElementById("moodBtnsContainer");
+
+if (moodBtnsContainer != null) {
+    moodBtnsContainer.style.visibility = "hidden";
+}
+
+const reportJokes: { joke: string, score: string, date: string }[] = [];
+
+const moodButtons = document.querySelectorAll('[data-score]');
+
+let previousJoke: string = "";
+
+let moodScoreButton: string = "";
+
+
+async function getJoke() {
+    const urlDadJoke: string = "https://icanhazdadjoke.com/";
+    const myHeaders = new Headers({
+        "Accept": "application/json",
+        "User-Agent": "My Library (https://github.com/Leptonita/sprint5)"
+    });
+
+    await fetch(urlDadJoke, {
+        method: 'GET',
+        headers: myHeaders
+    })
+        .then(response => response.json())
+        .then(json => {
+
+            if (moodBtnsContainer != null) {
+                moodBtnsContainer.style.visibility = "visible";
+            }
+
+            const divJoke: HTMLElement | null = document.querySelector("div .joke");
+
+            if (divJoke !== null) {
+
+                let currentJoke: string = json.joke;
+                //let lastScoreJoke = moodScoreButton[moodScoreButton.length - 1];
+                let lastScoreJoke = moodScoreButton;
+
+                getMoodScore();
+                const d = new Date();
+
+                reportJokes.push({ joke: previousJoke, score: lastScoreJoke, date: d.toISOString() });
+
+                previousJoke = currentJoke;
+                console.log(reportJokes);
+                return divJoke.innerHTML = json.joke;
+            } else {
+                return alert("sorry, something went wrong! No joke, no fun");
+            }
+        })
+        .catch(function (error) {
+            console.log("error", error);
+        });
+}
+
+
+
+function getMoodScore() {
+    moodScoreButton = "";
+    moodButtons.forEach(button =>
+        button.addEventListener('click', () => {
+            const moodScore: string | null = button.getAttribute("data-score");
+            console.log(moodScore);
+            if (moodScore !== null && (moodScore === "1" || moodScore === "2" || moodScore === "3")) {
+                moodScoreButton = moodScore;
+            } else {
+                moodScoreButton = "";
+            }
+
+        }));
+
+}
+
